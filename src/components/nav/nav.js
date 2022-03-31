a48f85fdf8026cbee06032cbf9e06c5a4d3a71ae
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-03-31 17:17:53 +0900

    add : nav layout

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -4,7 +4,12 @@ import styled from "styled-components";
 function nav() {
   return (
     <NavContainer>
-      <div>Logo</div>
+      <LogoWrapper>
+        <NavLogo
+          src="https://www.teamfresh.co.kr/resources/assets/img/logo.png"
+          alt=""
+        />
+      </LogoWrapper>
       <NavLinkList>
         <NavLinkButton>회사소개</NavLinkButton>
         <NavLinkButton>사업소개</NavLinkButton>
@@ -18,11 +23,39 @@ function nav() {
 export default nav;
 
 const NavContainer = styled.div`
+  width: 100%;
+  height: 60px;
   display: flex;
   justify-content: space-between;
-  width: 1400px;
-  border: 1px solid black;
+  position: fixed;
+  z-index: 10;
+  padding: 0.8rem;
+  background-color: white;
 `;
 
-const NavLinkList = styled.ul``;
-const NavLinkButton = styled.li``;
+const NavLogo = styled.img`
+  width: 110px;
+  margin-top: -10px;
+  padding-left: 10px;
+  padding-top: 9px;
+`;
+
+const LogoWrapper = styled.a`
+  display: inline-block;
+  padding-top: 0.3125rem;
+  padding-bottom: 0.3125rem;
+  margin-right: 1rem;
+  font-size: 1.25rem;
+  line-height: inherit;
+  white-space: nowrap;
+`;
+const NavLinkList = styled.ul`
+  display: flex;
+  padding-right: 20px;
+`;
+const NavLinkButton = styled.li`
+  padding: 0.5rem 1rem;
+  color: rgb(96, 96, 96);
+  font-weight: 500;
+  font-size: 0.9rem;
+`;
